1597fc82f65a7ddf171e628798741170a6523395
Kin Chang <changkinkwan@hotmail.com>
2022-06-22 14:57:46 +1000

    choice

--- Hackathon/ViewController/ChoiceViewController.swift
+++ Hackathon/ViewController/ChoiceViewController.swift
@@ -9,11 +9,30 @@ import Foundation
 
 class ChoiceViewController: UIViewController {
     override func viewDidLoad() {
+        checkDietaryProblem()
         //do other setup
     }
-    @IBAction func onScanClick(_ sender: Any) {
+    
+    func checkDietaryProblem (){
+            createAlert(title: "Before You Start", message: "Any dietary problem?")
     }
     
+    // Create alert for display
+    func createAlert(title:String, message:String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        let action = UIAlertAction(title: "Yes", style: .default) { (action) -> Void in
+            let toView = self.storyboard?.instantiateViewController(withIdentifier: "SettingsViewController") as! SettingsViewController
+            self.navigationController?.pushViewController(toView, animated: true)
+        }
+        
+        // Add action for user dismiss the alert by click "Yes"
+        alert.addAction(action)
+        
+        // Add action for user dismiss the alert by click "No"
+        alert.addAction(UIAlertAction(title: "No", style: UIAlertAction.Style.default, handler: { (action) in alert.dismiss(animated: true, completion:nil)}))
+        // Present the alert
+        self.present(alert, animated: true, completion: nil)
+    }
     
     
 }
